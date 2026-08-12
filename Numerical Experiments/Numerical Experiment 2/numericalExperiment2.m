dd9289b400a5a4ff72b0fb143231b0c3d2017fcb
Pablo Soto-Quiros <102713314+jusotoTEC@users.noreply.github.com>
2026-08-12 14:51:29 -0600

    Update numericalExperiment2.m

--- Numerical Experiments/Numerical Experiment 2/numericalExperiment2.m
+++ Numerical Experiments/Numerical Experiment 2/numericalExperiment2.m
@@ -1,4 +1,4 @@
-%% Generalized Low-Tubal-Rank Tensor Approximation under the M-Product
+%% A Closed-Form Solution for Generalized Low-Tubal-Rank Tensor Approximation
 % Numerical Experiment 2: Complex-Valued Tensors, associated with the paper:
 % "A Note on a Closed-Form Solution to the Generalized Low-Tubal-Rank Tensor Approximation Problem"
 %
